036a9871c3212e07d279cfa901a2ed68328c9e83
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-25 13:05:01 +0100

    Make riot observable public available as `control` getter / setter

--- src/playbooks/defaults.js
+++ src/playbooks/defaults.js
@@ -2,6 +2,7 @@ import hilightNode from '../utils/hilight/hilight_node.js'
 import unhilightNode from '../utils/hilight/unhilight_node.js'
 
 export default {
+  control: null,
   hilightNode,
   unhilightNode
 }
